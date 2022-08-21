febcf6f9191d358509825743d6c30216ea269b11
tom <toms@live.jp>
2022-08-21 09:18:03 +0900

    Command class is added

---
+++ src/command.ts
@@ -0,0 +1,14 @@
+import { exec } from "child_process";
+
+export class Command {
+  public static async execute(commandLine: string){
+    return new Promise(
+      (resolve: Function, reject: Function) =>
+        exec(
+          commandLine,
+          (err, stdout, stderr) =>
+            (err ? reject : resolve)(err ? stderr : stdout)
+        )
+    );
+  }
+}
