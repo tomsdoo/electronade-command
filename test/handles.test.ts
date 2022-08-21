ab603354ff55fcdfbd489e949641a0047ff81632
tom <toms@live.jp>
2022-08-21 09:28:26 +0900

    test for handles

---
+++ test/handles.test.ts
@@ -0,0 +1,26 @@
+import { describe, it } from "mocha";
+import { strict as assert } from "assert";
+
+import { handles, preloadObject } from "../src/";
+import { Command } from "../src/command";
+
+describe("handles", () => {
+  it("electronade-command:execute eventName exists", () => {
+    assert(
+      handles.find(
+        ({ eventName }) =>
+          eventName === "electronade-command:execute"
+      )
+    );
+  });
+
+  it("electronade-command:execute handler", async () => {
+    assert.equal(
+      await (handles.find(
+        ({ eventName }) =>
+          eventName === "electronade-command:execute"
+        ) as { handler: Function }).handler({}, "ls"),
+      await Command.execute("ls")
+    );
+  });
+});
