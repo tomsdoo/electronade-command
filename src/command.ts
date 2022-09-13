06e742b4d6ca7c4dcd4a0ca57ba893928fe7aa19
tomsdoo <32782986+tomsdoo@users.noreply.github.com>
2022-09-13 18:47:29 +0900

    eslint is added (#6)
    * eslint is installed
    
    * eslint is configured
    
    * npm script lint is added
    
    * lint fixed

--- src/command.ts
+++ src/command.ts
@@ -1,9 +1,11 @@
 import { exec } from "child_process";
 
+// eslint-disable-next-line @typescript-eslint/no-extraneous-class
 export class Command {
-  public static async execute(commandLine: string) {
-    return new Promise((resolve: Function, reject: Function) =>
+  public static async execute(commandLine: string): Promise<string | Buffer> {
+    return await new Promise((resolve: Function, reject: Function) =>
       exec(commandLine, (err, stdout, stderr) =>
+        // eslint-disable-next-line @typescript-eslint/strict-boolean-expressions
         (err ? reject : resolve)(err ? stderr : stdout)
       )
     );
